c7cbd8c06d8cd00e703dca80d8efedac861d0a3d
nazaninjahangiri85 <jahangirinazanin3@gmail.com>
2023-06-22 16:26:04 +0330

    version 1 of project

--- OnlineShop/src/main.js
+++ OnlineShop/src/main.js
@@ -1,14 +1,44 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// import './assets/main.css'
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import { fa } from "vuetify/iconsets/fa";
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
+// make sure to also import the coresponding css
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import "@fortawesome/fontawesome-free/css/all.css"; // Ensure your project is capable of handling css files
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: "light",
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+      fa,
+    },
+  },
+  components,
+  directives,
+});
+
+const app = createApp(App).use(vuetify)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
--- OnlineShop/src/views/HomeView.vue
+++ OnlineShop/src/views/HomeView.vue
@@ -1,9 +1,65 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      collapseOnScroll: true,
+      title: "Vue-2 Appssssssss",
+      cards: null
+  
+    };
+  },
+  mounted() {
+   this.getAndShowCards()
+  },
+  
+   methods: {
+    getAndShowCards() {
+      axios.get('https://fakestoreapi.com/products').then((response) => {
+        console.log(response.data);
+        this.cards = response.data;
+      })
+    }
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-card
+  class="mx-auto"
+  
+>
+  <v-container fluid>
+    <v-row dense dir="rtl" class="text-center">
+      <v-col
+        v-for="card in cards"
+        :key="card.title"
+        cols="3"
+      >
+        <v-card
+        class="mx-4 mb-4 pa-2"
+        height="90%"
+                rounded="lg"
+        >
+          <v-img
+                 class="ma-2"
+            :src="card.image"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          height="60%"
+          >
+           
+          </v-img>
+    <div class="title text-truncate">{{card.title}}</div>
+    <div class="title text-truncate mt-4" style="color: teal">{{card.price * 50000}} تومان</div>
+
+    
+
+    <v-btn class="mt-8 mb-2" color="orange">
+      خرید
+    </v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</v-card>
 </template>
